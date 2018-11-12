678c7c5745c3434f78d5f2c5a481865607e93411
John Palgut <jpalgut@grio.com>
2018-11-12 13:58:36 -0800

    WIP Decoder work

--- lib/decoding_deck.ex
+++ lib/decoding_deck.ex
@@ -8,25 +8,40 @@ defmodule ShopDeed.DecodingDeck do
 
   alias __MODULE__
 
-  @decoder_prefix Constants.prefix()
-  @decoder_version Constants.version()
-
   @primary_key false
   embedded_schema do
     field(:bytes, :binary)
     field(:total_bytes, :integer)
-    field(:version_and_heroes, :binary)
+    field(:version_and_heroes_byte, :binary)
+    field(:version, :integer)
+    field(:hero_count, :integer)
+    field(:checksum, :integer)
+    field(:card_bytes, :binary)
+    field(:name, :string)
+    field(:prefix, :string)
   end
 
   def changeset(bytes) do
     %DecodingDeck{}
-    |> cast(%{bytes: bytes, total_bytes: String.length(bytes)}, [:bytes, :total_bytes])
-    |> validate_length(:bytes, min: 10)
-    |> validate_prefix()
-    |> validate_version()
+    |> cast(%{}, [])
+    |> validate_prefix(bytes)
+    |> clean_and_decode_bytes()
+    |> split_bytes()
+    |> validate_number(:version, equal_to: Constants.version())
+    |> validate_checksum()
   end
 
-  defp clean_and_decode_bytes(%Ecto.Changeset{changes: %{bytes: bytes}} = changeset) do
+  defp validate_prefix(changeset, <<prefix::bytes-size(3)>> <> rest = bytes) do
+    case prefix == Constants.prefix() do
+      true ->
+        {put_change(changeset, :prefix, prefix), rest}
+
+      _ ->
+        {add_error(changeset, :prefix, "Must start with prefix '#{Constants.prefix()}'"), bytes}
+    end
+  end
+
+  defp clean_and_decode_bytes({changeset, bytes}) do
     clean_bytes =
       bytes
       |> String.replace("-", "/")
@@ -34,35 +49,43 @@ defmodule ShopDeed.DecodingDeck do
       |> Base.decode64()
 
     case clean_bytes do
-      :error -> add_error(changeset, :bytes, "Unable to base64 decode string: #{code}")
-      {:ok, bytes} -> put_change(changeset, :bytes, bytes)
+      :error -> {add_error(changeset, :bytes, "Unable to base64 decode string: #{bytes}"), bytes}
+      {:ok, decoded_bytes} -> {changeset, decoded_bytes}
     end
   end
 
-  defp validate_prefix(
-         %Ecto.Changeset{changes: %{bytes: <<prefix::bytes-size(3)>> <> rest}} = changeset
-       )
-       when prefix == @decoder_prefix do
-    put_change(changeset, :bytes, rest)
+  defp split_bytes(
+         {changeset,
+          <<version_and_heroes_byte::integer, checksum::integer, name_length::integer>> <> rest}
+       ) do
+    card_bytes_length = byte_size(rest) - name_length
+    <<card_bytes::bytes-size(card_bytes_length)>> <> name_bytes = rest
+
+    changeset
+    |> put_change(:version, version_and_heroes_byte >>> 4)
+    |> put_change(:hero_count, version_and_heroes_byte)
+    |> put_change(:checksum, checksum)
+    |> put_change(:card_bytes, card_bytes)
+    |> put_change(:name, name_bytes)
   end
 
-  defp validate_prefix(changeset) do
-    add_error(changeset, :bytes, "Must start with prefix '#{@decoder_prefix}'")
-  end
-
-  defp validate_version(
-         %Ecto.Changeset{changes: %{bytes: <<version_and_heroes::integer>> <> rest}} = changeset
-       )
-       when version_and_heroes >>> 4 == @decoder_version do
-    changeset |> put_change(:version_and_heroes, version_and_heroes) |> put_change(:bytes, rest)
-  end
-
-  defp validate_version(
+  defp validate_checksum(
          %Ecto.Changeset{
-           changes: %{bytes: <<version_and_heroes::integer>> <> _rest}
+           changes: %{
+             checksum: checksum,
+             card_bytes: card_bytes
+           },
+           valid?: true
          } = changeset
        ) do
-    IO.inspect(version_and_heroes)
-    add_error(changeset, :version_and_heroes, "Version mismatch: #{version_and_heroes >>> 4}")
+    computed_checksum =
+      card_bytes
+      |> String.codepoints()
+      |> Enum.reduce(0, fn <<int::integer>>, sum -> sum + int end) &&& 0xFF
+
+    case computed_checksum != checksum do
+      true -> add_error(changeset, :bytes, "Checksum mistach #{checksum} != #{computed_checksum}")
+      _ -> changeset
+    end
   end
 end
